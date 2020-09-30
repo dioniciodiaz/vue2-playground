bc38824be7fbd5ddaee89139727d61e6d50ab709
Dionicio Diaz <dionicio@geniusplaza.com>
2020-09-30 07:58:40 -0400

    add VuexPersistence to the store config

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+});
 
 Vue.use(Vuex);
+/**
+ * Dynamically import store elements.
+ */
+const files = require.context(".", false, /\.js$/);
+const modules = {};
+
+files.keys().forEach(key => {
+  if (key !== "./index.js") {
+    modules[key.replace(/(\.\/|\.js)/g, "")] = files(key).default;
+  }
+});
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules,
+  plugins: [vuexLocal.plugin]
 });
