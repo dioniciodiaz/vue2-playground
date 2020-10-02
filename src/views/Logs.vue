184835bea66a5b35a66e7dd1524e8cd0122a4f00
Dionicio Diaz <dionicio@geniusplaza.com>
2020-10-01 23:44:22 -0400

    rename logs component

--- src/views/Logs.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="logs">
-    <h1>This is an Logs page</h1>
-  </div>
-</template>
---
+++ src/views/Logs/List.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="logs container">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Sign In</th>
+          <th scope="col">Break</th>
+          <th scope="col">Lunch</th>
+          <th scope="col">Signing Off</th>
+        </tr>
+      </thead>
+      <tbody>
+        <AttendanceRow
+          v-for="attendance in activeAttendance"
+          :key="attendance.id"
+          :attendance="attendance"
+          @update-atendance="saveAttendance"
+        />
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  components: {
+    AttendanceRow: () =>
+      import(/* webpackChunkName: "AttendanceRow" */ "@v/Logs/Row.vue")
+  },
+  methods: {
+    ...mapActions({
+      updateAttendance: "Attendance/updateAttendance"
+    }),
+    saveAttendance(attendance) {
+      this.updateAttendance(attendance);
+    }
+  },
+  computed: {
+    ...mapGetters({
+      activeAttendance: "Attendance/activeAttendance"
+    })
+  }
+};
+</script>
+
+<style></style>
