432095a34952ef51535620332d9d7b0c0ff43b66
Dionicio Diaz <dionicio@geniusplaza.com>
2020-10-02 01:44:32 -0400

    wip

--- src/views/Logs/List.vue
+++ src/views/Logs/List.vue
@@ -23,6 +23,7 @@
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
+
 export default {
   components: {
     AttendanceRow: () =>
@@ -43,5 +44,3 @@ export default {
   }
 };
 </script>
-
-<style></style>
